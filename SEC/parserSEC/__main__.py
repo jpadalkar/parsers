1db7f37b3e787c2886ab8c7d04b9f98519e5bcf2
Jai Padalkar <jpadalkar@gmail.com>
2019-05-09 14:40:36 -0400

    main changes

--- SEC/parserSEC/__main__.py
+++ SEC/parserSEC/__main__.py
@@ -1,8 +1,14 @@
 """Main script to run entire package"""
 
+from parserSEC import crawl
+
 def main():
     """Top level command line interface."""
-    print("Hello world!")
+    print("SEC Parser")
+    fin_statements_url = "https://www.sec.gov/dera/data/financial-statement-data-sets.html"
+    crowdfunding_url = "https://www.sec.gov/dera/data/crowdfunding-offerings-data-sets"
+    mutual_fund_url = "https://www.sec.gov/dera/data/mutual-fund-prospectus-risk-return-summary-data-sets"
+    
 
 
 if __name__ == "__main__":
