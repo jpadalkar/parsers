c88ea3439cf83884469a775ecfcf33f6ca1b2655
Jai Padalkar <jpadalkar@gmail.com>
2019-05-09 11:44:29 -0400

    Added package setup config

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+    SEC parser python package configuration.
+"""
+
+from setuptools import setup
+
+setup(
+      name='parserSEC',
+      version='0.1.0',
+      packages=['parserSEC'],
+      include_package_data=True,
+      install_requires=[
+                        'click==6.7',
+                        'sh==1.12.14',
+                        'flask==0.12.2',
+                        'arrow==0.10.0',
+                        'html5validator==0.2.8',
+                        'pycodestyle==2.3.1',
+                        'pydocstyle==2.0.0',
+                        'pylint==2.1.1',
+                        'sh==1.12.14',
+                        ],
+      entry_points={
+      'console_scripts': [
+                          'parserSEC = parserSEC.__main__:main'
+                          ]
+      },
+      )
